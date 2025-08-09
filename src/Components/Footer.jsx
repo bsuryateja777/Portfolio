0ec804175a548ae541ad532a8900a4f3d34c6adf
bsuryateja777 <bsuryateja777@gmail.com>
2025-08-09 20:44:41 +0530

    updated links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -26,42 +26,42 @@ export default function Footer() {
             <div className="flex flex-wrap items-center justify-center gap-4 lg:gap-16 mt-3">
                 <div className="flex items-center gap-2">
                     <Mail className="size-4 lg:size-5 2xl:size-6" />
-                    <a href="mailto:bsuryateja777@gmail.com" className="hover:underline font-ovo">
+                    <a href="mailto:bsuryateja777@gmail.com" className="hover:underline font-ovo" target="_blank" rel="noopener noreferrer" >
                         bsuryateja777@gmail.com
                     </a>
                 </div>
 
                 <div className="flex items-center gap-2">
                     <img src={LinkedIn} alt="" className='w-4' />
-                    <a href="https://www.linkedin.com/in/barla-surya-teja-reddy-05b6741a5?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" className="hover:underline font-ovo">
+                    <a href="https://www.linkedin.com/in/barla-surya-teja-reddy-05b6741a5?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" className="hover:underline font-ovo" target="_blank" rel="noopener noreferrer" >
                         LinkedIn
                     </a>
                 </div>
 
                 <div className="flex items-center gap-2">
                     <img src={GitHub} alt="" className='w-5 relative bottom-0.5' />
-                    <a href="https://github.com/bsuryateja777/" className="hover:underline font-ovo">
+                    <a href="https://github.com/bsuryateja777/" className="hover:underline font-ovo" target="_blank" rel="noopener noreferrer" >
                         GitHub
                     </a>
                 </div>
 
                 <div className="flex items-center gap-2">
                     <img src={Instagram} alt="" className='w-5' />
-                    <a href="https://www.instagram.com/___suryaaateja_?igsh=OWJkaXF5djNleHQ4&utm_source=qr" className="hover:underline font-ovo">
+                    <a href="https://www.instagram.com/___suryaaateja_?igsh=OWJkaXF5djNleHQ4&utm_source=qr" className="hover:underline font-ovo" target="_blank" rel="noopener noreferrer" >
                         Instagram
                     </a>
                 </div>
 
                 <div className="flex items-center gap-2">
                     <img src={Twitter} alt="" className='w-5 relative' />
-                    <a href="https://x.com/_suryateja_777?s=21" className="hover:underline font-ovo">
+                    <a href="https://x.com/_suryateja_777?s=21" className="hover:underline font-ovo" target="_blank" rel="noopener noreferrer" >
                         Twitter
                     </a>
                 </div>
 
                 <div className="flex items-center gap-2">
                     <img src={LeetCode} alt="" className='w-5 relative bottom-0.5' />
-                    <a href="https://leetcode.com/u/surya_teja_reddy_777/" className="hover:underline font-ovo">
+                    <a href="https://leetcode.com/u/surya_teja_reddy_777/" className="hover:underline font-ovo" target="_blank" rel="noopener noreferrer" >
                         LeetCode
                     </a>
                 </div>
