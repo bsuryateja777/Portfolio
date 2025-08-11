71ca6d8d74047b5470f8ff24312c523d6cf8d936
Reddy <bsuryateja777@gmail.com>
2025-08-11 19:58:55 +0530

    animations 08/11

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -43,14 +43,14 @@ export default function Contact() {
 
     return (
         <motion.div id="contact" className='w-full px-[6%] lg:px-[12%] mt-20 py-10 pb-34 md:pb-2 2xl:pb-2.5 scroll-mt-8 lg:scroll-mt-10 2xl:scroll-mt-14 bg-header-bg bg-no-repeat  bg-cover bg-center' initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.5, delay: 0.3 }}>
-            <motion.h4 className='text-center mb-2 text-md 2xl:text-lg font-ovo' initial={{ opacity: 0, y: -10 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1 }}>
+            <motion.h4 className='text-center mb-2 text-md 2xl:text-lg font-ovo' initial={{ opacity: 0, y: -10 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
                 Connect With Me
             </motion.h4>
-            <motion.h2 className='text-center text-4xl 2xl:text-5xl font-ovo overflow-hidden' initial={{ opacity: 0, y: -10 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1 }}>
+            <motion.h2 className='text-center text-4xl 2xl:text-5xl font-ovo overflow-hidden' initial={{ opacity: 0, y: -10 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
                 Get in Touch
             </motion.h2>
 
-            <motion.p className='text-center max-w-xl mx-auto mt-4 2xl:mt-5 mb-8  2xl:mb-12 font-ovo' initial={{ opacity: 0, y: -10 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1 }}>
+            <motion.p className='text-center max-w-xl mx-auto mt-4 2xl:mt-5 mb-8  2xl:mb-12 font-ovo' initial={{ opacity: 0, y: -10 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
                 {!result ?
                     "I'd love to hear from you! If you have any questions, comments, or feedback, Please use the form below."
                     :
@@ -59,14 +59,14 @@ export default function Contact() {
             </motion.p>
 
             <form onSubmit={sendEmail} className='max-w-2xl mx-auto mt-16 lg:mt-0'>
-                <motion.div className='grid grid-cols-auto md:grid-cols-2 gap-6 mt-6 mb-4 2xl:mt-10 2xl:mb-8' initial={{ opacity: 0, y: 10 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1 }}>
+                <motion.div className='grid grid-cols-auto md:grid-cols-2 gap-6 mt-6 mb-4 2xl:mt-10 2xl:mb-8' initial={{ opacity: 0, y: 10 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
                     <input type="text" name='name' value={name} onChange={(e) => setName(e.target.value)} placeholder='Enter your name' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' />
                     <input type="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Enter your email' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' />
                 </motion.div>
                 <motion.textarea name='message' value={message} onChange={(e) => setMessage(e.target.value)} placeholder='Enter your message' required className='min-h-[16rem] lg:min-h-[10rem] 2xl:min-h-[14rem] resize-none w-full px-3 py-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'
-                    initial={{ opacity: 0, y: 10 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1 }} />
+                    initial={{ opacity: 0, y: 10 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} />
                 <motion.button type='submit' className='mt-5 md:mt-0 py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/85 text-white rounded-full mx-auto hover:bg-black duration-500'
-                whileTap={{scale:0.70}}>
+                whileTap={{scale:0.70}} initial={{y:5, opacity:0}} whileInView={{y:0, opacity: 1}} transition={{duration:0.5, delay:0.25   }}>
                     Submit now<RightArrow />
                 </motion.button>
 
